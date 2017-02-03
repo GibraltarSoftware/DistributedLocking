279cfae78ce67ef0d308ab606c6674c3422965a9
Kendall Miller <kendall@gibraltarsoftware.com>
2017-02-03 16:40:58 -0500

    Update file headers with new open source license.

--- src/DistributedLocking/Internal/CommonCentralLogic.cs
+++ src/DistributedLocking/Internal/CommonCentralLogic.cs
@@ -1,18 +1,22 @@
-﻿#region File Header
-// /********************************************************************
-//  * COPYRIGHT:
-//  *    This software program is furnished to the user under license
-//  *    by Gibraltar Software Inc, and use thereof is subject to applicable 
-//  *    U.S. and international law. This software program may not be 
-//  *    reproduced, transmitted, or disclosed to third parties, in 
-//  *    whole or in part, in any form or by any manner, electronic or
-//  *    mechanical, without the express written consent of Gibraltar Software Inc,
-//  *    except to the extent provided for by applicable license.
-//  *
-//  *    Copyright © 2008 - 2015 by Gibraltar Software, Inc.  
-//  *    All rights reserved.
-//  *******************************************************************/
+﻿#region File Header and License
+// /*
+//    CommonCentralLogic.cs
+//    Copyright 2008-2017 Gibraltar Software, Inc.
+//    
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// */
 #endregion
+
 using System;
 using System.Diagnostics;
 
