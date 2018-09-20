568ccc5e74d8a6405cf323577f4ba9e4f54d73f5
Kendall Miller <kendall@gibraltarsoftware.com>
2018-09-20 18:56:46 -0400

    Add distributed locking console demo app

---
+++ src/DistributedLocking.Test.Console/LockingClient.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gibraltar.DistributedLocking.Test.Console
+{
+    /// <summary>
+    /// Generates locking requests for resources and releases them after a period of time.
+    /// </summary>
+    public class LockingClient
+    {
+        private readonly DistributedLockManager _lockManager;
+        private readonly RandomNumberGenerator _rng;
+        private readonly TimeSpan _maxLockDuration;
+        private readonly TimeSpan _lockTimeout;
+        private readonly string _lockPrefix;
+        private readonly int _maxLockNumber;
+
+        private CancellationTokenSource _cancellationTokenSource;
+        private Task[] _clientTasks;
+
+        public LockingClient(DistributedLockManager lockManager, RandomNumberGenerator rng, TimeSpan maxLockDuration, TimeSpan lockTimeout, string lockPrefix, int maxLockNumber)
+        {
+            _lockManager = lockManager;
+            _rng = rng;
+            _maxLockDuration = maxLockDuration;
+            _lockTimeout = lockTimeout;
+            _lockPrefix = lockPrefix;
+            _maxLockNumber = maxLockNumber;
+        }
+
+        /// <summary>
+        /// Start processing locks with the specified number of locking tasks
+        /// </summary>
+        /// <param name="tasks"></param>
+        public void Start(int tasks)
+        {
+            if (_cancellationTokenSource != null)
+                throw new InvalidOperationException("Client is already started");
+
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            _clientTasks = new Task[ tasks ];
+
+            for (var index = 0; index < _clientTasks.Length; index++)
+            {
+                _clientTasks[index] = Task.Run(() => GenerateLocks(_cancellationTokenSource), _cancellationTokenSource.Token);
+            }
+        }
+
+        /// <summary>
+        /// Stop processing locks (releasing any locks held)
+        /// </summary>
+        public void Stop()
+        {
+            if (_cancellationTokenSource != null)
+            {
+                _cancellationTokenSource.Cancel();
+
+                _cancellationTokenSource = null;
+
+            }
+
+            _clientTasks = null;
+        }
+
+        private int GetRandomNumber(int minValue, int maxValue)
+        {
+            lock(_rng)
+            {
+                var randomBytes = new Byte[ 4 ];
+                _rng.GetBytes(randomBytes);
+                var randomInt = Math.Abs( BitConverter.ToInt32(randomBytes, 0));
+
+                if (randomInt != 0)
+                {
+                    randomInt = minValue + (randomInt  % (maxValue - minValue));
+                }
+
+                return randomInt;
+            }
+        }
+
+        private void GenerateLocks(CancellationTokenSource cancellationTokenSource)
+        {
+            try
+            {
+                while (cancellationTokenSource.IsCancellationRequested == false)
+                {
+                    var lockTimespan = new TimeSpan(GetRandomNumber(0, (int)_maxLockDuration.Ticks));
+                    var nameIndex = GetRandomNumber(1, _maxLockNumber);
+                    var name = _lockPrefix + "-" + nameIndex;
+                    try
+                    {
+                        var stopwatch = Stopwatch.StartNew();
+                        using (var newLock = _lockManager.Lock(this, name, (int)_lockTimeout.TotalSeconds))
+                        {
+                            stopwatch.Stop();
+
+                            System.Console.WriteLine("{4} Thread {1} - Acquired lock {0} in {2:N0}ms, will hold for {3:N0}ms", 
+                                newLock.Name, Thread.CurrentThread.ManagedThreadId, stopwatch.ElapsedMilliseconds, lockTimespan.TotalMilliseconds, DateTime.Now);
+
+                            Task.Delay(lockTimespan, cancellationTokenSource.Token).Wait();
+
+                            System.Console.WriteLine("{2} Thread {1} - Releasing lock {0}", newLock.Name, Thread.CurrentThread.ManagedThreadId, DateTime.Now);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Console.WriteLine("{3} Thread {1} - Unable to acquire lock {0} due to {2}", 
+                            name, Thread.CurrentThread.ManagedThreadId, ex.GetBaseException().GetType(), DateTime.Now);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("{3} Thread {1} - Locking task failed due to {0}:\r\n{2}", 
+                    ex.GetBaseException().GetType(), Thread.CurrentThread.ManagedThreadId, ex.StackTrace, DateTime.Now);
+            }
+        }
+    }
+}
---
+++ src/DistributedLocking.Test.Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace Gibraltar.DistributedLocking.Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuredConnectionString = ConfigurationManager.ConnectionStrings["LockManager"];
+
+            if (configuredConnectionString == null)
+            {
+                System.Console.WriteLine("No connection string configured named 'LockManager'");
+                return;
+            }
+
+            var lockProvider = new SqlLockProvider(configuredConnectionString.ConnectionString);
+            var lockManager = new DistributedLockManager(lockProvider);
+
+            System.Console.WriteLine("Configuring Test");
+
+            var rng = new RNGCryptoServiceProvider();
+
+            var lockingClient = new LockingClient(lockManager, rng, new TimeSpan(0,0,10), new TimeSpan(0,2,00), "Session~d9a84ccf-9bef-4777-b202-a4343d35089a", 250);
+
+            try
+            {
+                lockingClient.Start(100);
+                System.Console.WriteLine("Running Lock Test, press any key to exit");
+                System.Console.ReadKey(true);
+
+                System.Console.WriteLine("Shutting down lock test");
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+            finally
+            {
+                lockingClient.Stop();
+                Thread.Sleep(new TimeSpan(0, 0, 10));
+                System.Console.WriteLine("Exiting Lock Test");                
+            }
+        }
+    }
+}
